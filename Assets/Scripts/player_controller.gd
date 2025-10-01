72d2bb46f5179b901dfc7d8cc66a63f6dcbafd64
Tanklover27 <145925420+Equuscabalus@users.noreply.github.com>
2025-10-01 12:06:47 +0700

    Jump Down mechanism

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -13,15 +13,22 @@ var speed_multiplier = 30.0
 #const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
+func _input(event):
+	# Handle jump.
+	if event.is_action_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		#Handle Jump Down
+	if event.is_action_pressed("Move Down"):
+		set_collision_mask_value(10,false)
+	else:
+		set_collision_mask_value(10,true)
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
