1d93b4cba3b45ec1f51a53b26540cffa5d00158e
Tanklover27 <145925420+Equuscabalus@users.noreply.github.com>
2025-10-02 21:52:08 +0700

    added jump refill

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -9,7 +9,9 @@ var direction = 0
 
 var speed_multiplier = 30.0
 
-var has_spam = true
+var can_jump = true
+
+@export var spam_ammount=0
 
 #const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
@@ -18,12 +20,14 @@ func _input(event):
 	# Handle jump.
 	if event.is_action_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		can_jump=true
 	# Handle spam jump
-	elif event.is_action_pressed("Jump") and has_spam and not is_on_floor():
+	elif event.is_action_pressed("Jump") and can_jump and spam_ammount>0 and not is_on_floor():
 		var spam = spam_scene.instantiate()
 		spam.position = position + Vector2(0, 5.0)
 		get_parent().add_child(spam)
-		has_spam = false
+		can_jump = false
+		spam_ammount=spam_ammount-1
 		velocity.y = JUMP_VELOCITY
 	#Handle Jump Down
 	if event.is_action_pressed("Move Down"):
@@ -36,8 +40,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis("Move Left", "Move Right")
