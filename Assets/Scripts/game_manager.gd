f8b3d588e0a2b51ee3370b0ba30cf772f4b426ad
ShallowMelon <wonovincent6@gmail.com>
2025-10-04 20:02:07 +0700

    Need item to open objective system

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -3,9 +3,24 @@ extends Node
 var current_area = 1
 var area_path = "res://Assets/Scenes/Area/"
 
+var spam = 0
+
+func addspam():
+	spam += 1
+	if spam == 1 :
+		var portal = get_tree().get_first_node_in_group("Area_Exits") as AreaExit
+		portal.open()
+
 func next_level():
 	current_area += 1
 	var full_path = area_path + "area_" + str(current_area) + ".tscn"
 	#get_tree().change_scene_to_file("res://Assets/Scenes/Area/area_2.tscn")
 	get_tree().change_scene_to_file(full_path)
 	print("The player have completed the objective!"+str(current_area))
+	setuparea()
+
+func setuparea():
+	resetcount()
+	
+func resetcount():
+	spam == 0
--- Assets/Scripts/pickablespam.gd
+++ Assets/Scripts/pickablespam.gd
@@ -3,4 +3,5 @@ extends Area2D
 func _on_body_entered(body):
 	if body is PlayerController:
 		body.spam_ammount += 1
+		GameManager.addspam()
 		queue_free() 
